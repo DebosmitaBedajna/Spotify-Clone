c82bc9adc7197ecd35eb7491ff5d5a3adf3d3ef0
Debosmita Bedajna <bedajnadebosmita@gmail.com>
2022-12-09 20:17:09 +0530

    Finally the song will Play

--- scipt.js
+++ scipt.js
@@ -1,3 +1,31 @@
 console.log("Stopify- Where music don't stop");
+
+//Initialization of all the elements that we will be using 
+let songIndex=0;
 let Audioelement= new Audio('Nothing.mp3');
-Audioelement.play()
+let Play=document.getElementById('Play');
+let MyProgressBar=document.getElementById('Progress');
+
+//Initialization of all the songs that is in our playlist
+let songs=[
+    {songName="Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"},
+    {songName="Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"},
+    {songName="Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"},
+    {songName="Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"},
+    {songName="Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"},
+    {songName="Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"},
+    {songName="Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"}
+]
+
+
+//The song will start playing when you will click this button 
+Play.addEventListener('click',()=>{
+    if(Audioelement.paused|| Audioelement.currentTime<=0)
+    {
+        Audioelement.play();
+    }
+})
+//The Progress Bar will get updated as soon as you play the song
+MyPrograssBar.addEventListener('timeupdate',()=>{
+    console.log("The time is getting updated");
+})
