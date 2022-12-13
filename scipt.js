08f5eccf98460379e74c760a5f03092d6b21154e
Debosmita Bedajna <bedajnadebosmita@gmail.com>
2022-12-13 17:21:24 +0530

    corr

--- scipt.js
+++ scipt.js
@@ -12,7 +12,7 @@ let songItems= Array.from(document.getElementsByClassName('songItem'));
 let songs=[
     {songName:"Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"},
     {songName:"Cold/Mess", filepath:"cold-mess.mp3",coverpath:"Cold-mess.jpeg"},
-    {songName:"Night Changes", filepath:"NightChanges.mp3",coverpath:"NightChanges.jpeg"},
+    {songName:"Night Changes", filepath:"NightChanges.mp3",coverpath:"Nightchanges.jpeg"},
     {songName:"Tere Hi Hum", filepath:"tere hi hum.mp3",coverpath:"Tere hi hum.jpeg"},
     {songName:"Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"},
     {songName:"Nothing", filepath:"nothing.mp3",coverpath:"Nothingcover.jpg"},
